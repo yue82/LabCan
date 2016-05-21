d08ce4a39f131d11e0207d2a322399cbab0e3450
tktr <no.82.vuf.lab@gmail.com>
2016-05-21 20:14:33 +0900

    complete user edit

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,9 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  def current_user?(user)
+    user == current_user
+  end
 
   def current_user
     if (user_id = session[:user_id])
@@ -40,4 +43,12 @@ module SessionsHelper
     @current_user = nil
   end
 
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
 end
