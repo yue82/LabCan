b1752317941035e5249b6a30a11c345bb37ed869
tktr <no.82.vuf.lab@gmail.com>
2016-05-21 17:27:42 +0900

    fix routes to align

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   root            'static_pages#home'
-  get 'signup' => 'users#new'
+  get    'signup' => 'users#new'
   get    'login'  => 'sessions#new'
   post   'login'  => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
