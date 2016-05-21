57784a97408ab2a799de53bcf648c2c963c535fd
tktr <no.82.vuf.lab@gmail.com>
2016-05-21 21:42:55 +0900

    finish user del and index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :index]
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def show
     @user = User.find(params[:id])
@@ -40,6 +41,13 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = "\"" + @user.name + "\" deleted"
+    redirect_to users_url
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email,
@@ -57,6 +65,10 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 
 end
