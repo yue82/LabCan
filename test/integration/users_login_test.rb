9daa98d199c81bf2f72100641c6702e77727b56a
tktr <no.82.vuf.lab@gmail.com>
2016-05-21 17:56:19 +0900

    add logout function

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -18,9 +18,10 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path, count: 0
   end
 
-  test "login with valid information" do
+  test "login with valid information " do
     get login_path
     post login_path, session: {email: @user.email, password: 'password'}
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
@@ -28,5 +29,20 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
   end
+
+  test "login and logout with valid information " do
+    get login_path
+    post login_path, session: {email: @user.email, password: 'password'}
+    assert_redirected_to @user
+    follow_redirect!
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
 
 end
