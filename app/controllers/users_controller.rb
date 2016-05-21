11b9bc4fdf84caf9b686634c6ca21def130a3cb2
tktr <no.82.vuf.lab@gmail.com>
2016-05-21 16:43:41 +0900

    fin signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,24 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if
+      @user.save
+      flash[:success] = "Welcom to the LabCan!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email,
+                                 :password, :password_confirmation)
+  end
+
 end
