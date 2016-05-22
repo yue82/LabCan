63688c34b409ba68a639fcf0d2c381cd4df0162f
tktr <no.82.vuf.lab@gmail.com>
2016-05-22 16:05:45 +0900

    refactoring account activate

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,15 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
   def downcase_email
     self.email = email.downcase
