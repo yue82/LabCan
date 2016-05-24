0d42179b53b54aefae9d5df546fcf4fa622e7688
yue82 <no.82.vuf.lab@gmail.com>
2016-05-24 13:20:51 +0900

    add attendance controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,14 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    @attendance = Attendance.new
   end
 
   def create
     @user = User.new(user_params)
+    @attendance = Attendance.new
     if @user.save
+      @attendance.save
       @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
@@ -44,6 +47,8 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
+    @attendance = Attendance.find(params[:id])
+    @attendance.destroy
     flash[:success] = "\"" + @user.name + "\" deleted"
     redirect_to users_url
   end
