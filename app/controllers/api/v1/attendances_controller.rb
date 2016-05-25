65f3bda406eabd94561cd908d929a9188ccc7a12
yue82 <no.82.vuf.lab@gmail.com>
2016-05-25 19:25:45 +0900

    fix model to fit test attendance tests are pending

--- app/controllers/api/v1/attendances_controller.rb
+++ app/controllers/api/v1/attendances_controller.rb
@@ -1,37 +1,43 @@
 module Api
   module V1
     class AttendancesController < ApplicationController
+      before_action :correct_user, only: [:new, :destroy]
 
       def new
         user = User.find_by(check_token: params[:token])
-        if user && user.activated?
-          if !user.attendance.attend
-            msg = { msg: "Welcom to Lab" }
-            user.attendance.checkin
-          else
-            msg = { msg: "Already checked in" }
-          end
+        if !user.attendance.attend
+          msg = { msg: "Welcom to Lab" }
+          status = :success
+          user.attendance.checkin
         else
-          msg = { msg: "Bad user or token" }
+          msg = { msg: "Already checked in" }
+          status = :noupdate
         end
-        render json: msg
+        render json: msg, status: status
       end
 
       def destroy
         user = User.find_by(check_token: params[:token])
-        if user && user.activated?
-          if user.attendance.attend
-            msg = { msg: "Goodbye" }
-            user.attendance.checkout
-          else
-            msg = { msg: "Already checked out" }
-          end
+        if user.attendance.attend
+          msg = { msg: "Goodbye" }
+          status = :success
+          user.attendance.checkout
         else
-          msg = { msg: "Bad user or token" }
+          msg = { msg: "Already checked out" }
+          status = :noupdate
         end
-        render json: msg
+        render json: msg, status: status
       end
 
+      private
+      def correct_user
+        user = User.find_by(check_token: params[:token])
+        if !user || !user.activated?
+          msg = { msg: "Bad user or token" }
+          status = :error
+          render json: msg, status: status
+        end
+      end
     end
   end
 end
